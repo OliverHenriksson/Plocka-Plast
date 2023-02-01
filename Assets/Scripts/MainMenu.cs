761942ba7ab8c8c9d2f05223c58510e2762860d6
MelvinV04 <79506720+MelvinV04@users.noreply.github.com>
2023-02-01 10:22:17 +0100

    kod

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,15 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class MainMenu : MonoBehaviour
+public class MainMenu : MonoBehaviour 
 {
+    //Gör så man kan starta och avsluta spelet från menyn -Melvin
    public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //Man byter till startscenen -Melvin
     }
 
     public void QuitGame()
     {
-        Application.Quit();
+        Application.Quit(); //Spelet avslutas -Melvin
     }
 }
