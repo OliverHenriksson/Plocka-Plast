2f7c0b0af2f642848dcfefde98a3b3727ca17c5b
MelvinV04 <79506720+MelvinV04@users.noreply.github.com>
2023-02-01 09:29:24 +0100

    Kommentering

--- Assets/Scripts/Dörr.cs
+++ Assets/Scripts/Dörr.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Dörr : MonoBehaviour
 {
+    //Den här koden gör så att man kan gå ut och in i affären -Melvin
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.name == "Player" && gameObject.name == "In")
+        if (collision.gameObject.name == "Player" && gameObject.name == "In") //Om objektet som heter"Player" kolliderar med objektet som heter "In" så ändras scen +1 så man kommer in
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
 
-        if(collision.gameObject.name == "Player" && gameObject.name == "Ut")
+        if (collision.gameObject.name == "Player" && gameObject.name == "Ut") //Om objektet som heter"Player" kolliderar med objektet som heter "Ut" så ändras scen -1 så man kommer ut
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+            }
         }
     }
 }
